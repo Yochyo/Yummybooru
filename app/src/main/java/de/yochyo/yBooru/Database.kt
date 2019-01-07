df1f1685e820d4ea2fbab9b55139fecc254a2931
Yochyo <herodragon25@gmail.com>
2019-01-07 13:45:43 +0100

    create database

--- app/src/main/java/de/yochyo/yBooru/Database.kt
+++ app/src/main/java/de/yochyo/yBooru/Database.kt
@@ -3,12 +3,18 @@ package de.yochyo.yBooru
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import org.jetbrains.anko.db.*
+import java.text.DateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Database(context: Context) : ManagedSQLiteOpenHelper(context, "database", null, 1) {
     private val TABLE_TAGS = "tags"
+    private val TABLE_SUBSCRIPTION = "subs"
     private val COLUMN_NAME = "name"
     private val COLUMN_IS_FAVORITE = "isFavorite"
-    private val COLUMN_IS_SUBSCRIBED = "isSubscribed"
+    private val COLUMN_DATE = "Date"
+    private val COLUMN_SUBSCRIBED_SINCE = "start"
+    private val COLUMN_SUBSCRIBED_STATUS = "current"
 
     companion object {
         private var _instance: Database? = null
@@ -19,20 +25,35 @@ class Database(context: Context) : ManagedSQLiteOpenHelper(context, "database", 
     }
 
     private val tags = ArrayList<Tag>()
+    private val subs = ArrayList<Subscription>()
 
     init {
         tags += getTags()
+        subs += getSubscriptions()
     }
 
 
-    fun addTag(tag: Tag) {
+    fun addTag(name: String, isFavorite: Boolean) {
+        val tag = Tag(name, isFavorite, Date())
         if (tags.find { it.name == tag.name } == null) {
             tags += tag
             use {
                 insert(TABLE_TAGS,
                         COLUMN_NAME to tag.name,
-                        COLUMN_IS_FAVORITE to if (tag.isFavorite) 1 else 0,
-                        COLUMN_IS_SUBSCRIBED to if (tag.isSubscribed) 1 else 0)
+                        COLUMN_IS_FAVORITE to if (tag.isFavorite) 1 else 0)
+            }
+        }
+    }
+
+    fun addSubscription(name: String, startID: Int) {
+        val sub = Subscription(name, startID, startID)
+        if (subs.find { it.name == name } == null) {
+            subs += sub
+            use {
+                insert(TABLE_SUBSCRIPTION,
+                        COLUMN_NAME to name,
+                        COLUMN_SUBSCRIBED_SINCE to startID,
+                        COLUMN_SUBSCRIBED_SINCE to startID)
             }
         }
     }
@@ -42,7 +63,17 @@ class Database(context: Context) : ManagedSQLiteOpenHelper(context, "database", 
         if (tag != null) {
             tags.remove(tag)
             use {
-                delete(TABLE_TAGS, whereClause = "id = {name}", args = *arrayOf("name" to name))
+                delete(TABLE_TAGS, whereClause = "$COLUMN_NAME = {name}", args = *arrayOf("name" to name))
+            }
+        }
+    }
+
+    fun removeSubscription(name: String) {
+        val sub = subs.find { it.name == name }
+        if (sub != null) {
+            subs.remove(sub)
+            use {
+                delete(TABLE_SUBSCRIPTION, whereClause = "$COLUMN_NAME = {name}", args = *arrayOf("name" to name))
             }
         }
     }
@@ -51,36 +82,62 @@ class Database(context: Context) : ManagedSQLiteOpenHelper(context, "database", 
         val tag = tags.find { it.name == changedTag.name }
         if (tag != null) {
             tag.isFavorite = changedTag.isFavorite
-            tag.isSubscribed = changedTag.isSubscribed
             use {
                 update(TABLE_TAGS,
-                        COLUMN_IS_FAVORITE to if (changedTag.isFavorite) 1 else 0,
-                        COLUMN_IS_SUBSCRIBED to if (changedTag.isSubscribed) 1 else 0).whereArgs("$COLUMN_NAME = {name}", "name" to changedTag.name).exec()
+                        COLUMN_IS_FAVORITE to if (changedTag.isFavorite) 1 else 0)
+                        .whereArgs("$COLUMN_NAME = {name}", "name" to changedTag.name).exec()
             }
         }
     }
 
-    fun getTag(name: String): Tag? {
-        return getTags().find { it.name == name }
+    fun changeSubscription(changedSub: Subscription) {
+        val sub = subs.find { it.name == changedSub.name }
+        if (sub != null) {
+            sub.currentID = changedSub.currentID
+            use {
+                update(TABLE_SUBSCRIPTION,
+                        COLUMN_SUBSCRIBED_STATUS to changedSub.currentID)
+                        .whereArgs("$COLUMN_NAME = {name}", "name" to changedSub.name).exec()
+            }
+        }
     }
+
+    fun getSubscription(name: String) = getSubscriptions().find { it.name == name }
+    fun getTag(name: String) = getTags().find { it.name == name }
 
     fun getTags(): List<Tag> {
         if (tags.isNotEmpty()) return tags
         else return use {
             val t = ArrayList<Tag>()
-            select(TABLE_TAGS, COLUMN_IS_FAVORITE, COLUMN_NAME).parseList(object : MapRowParser<ArrayList<Tag>> {
+            select(TABLE_TAGS, COLUMN_NAME, COLUMN_IS_FAVORITE, COLUMN_DATE).parseList(object : MapRowParser<ArrayList<Tag>> {
                 override fun parseRow(columns: Map<String, Any?>): ArrayList<Tag> {
-                    columns[COLUMN_NAME]
                     val name = columns[COLUMN_NAME].toString()
                     val isFavorite = columns[COLUMN_IS_FAVORITE] as Int
-                    val isSubscribed = columns[COLUMN_IS_SUBSCRIBED] as Int
-                    t += Tag(name, isFavorite == 1, isSubscribed == 1)
+                    val date = DateFormat.getInstance().parse(columns[COLUMN_DATE].toString())
+
+                    t += Tag(name, isFavorite == 1, date)
                     return t
                 }
             })
             t
         }
+    }
 
+    fun getSubscriptions(): List<Subscription> {
+        if (subs.isNotEmpty()) return subs
+        else return use {
+            val s = ArrayList<Subscription>()
+            select(TABLE_SUBSCRIPTION, COLUMN_NAME, COLUMN_SUBSCRIBED_SINCE, COLUMN_SUBSCRIBED_STATUS).parseList(object : MapRowParser<ArrayList<Subscription>> {
+                override fun parseRow(columns: Map<String, Any?>): ArrayList<Subscription> {
+                    val name = columns[COLUMN_NAME].toString()
+                    val startID = columns[COLUMN_SUBSCRIBED_SINCE] as Int
+                    val currentID = columns[COLUMN_SUBSCRIBED_STATUS] as Int
+                    s += Subscription(name, startID, currentID)
+                    return s
+                }
+            })
+            s
+        }
     }
 
 
@@ -88,15 +145,19 @@ class Database(context: Context) : ManagedSQLiteOpenHelper(context, "database", 
         db.createTable(TABLE_TAGS, true,
                 COLUMN_NAME to TEXT + PRIMARY_KEY + UNIQUE,
                 COLUMN_IS_FAVORITE to INTEGER,
-                COLUMN_IS_SUBSCRIBED to INTEGER)
+                COLUMN_DATE to TEXT)
+        db.createTable(TABLE_SUBSCRIPTION, true,
+                COLUMN_NAME to TEXT + PRIMARY_KEY + UNIQUE,
+                COLUMN_SUBSCRIBED_SINCE to INTEGER,
+                COLUMN_SUBSCRIBED_STATUS to INTEGER)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
 }
 
 val Context.database: Database
     get() = Database.getInstance(this)
 
-class Tag(val name: String, var isFavorite: Boolean, var isSubscribed: Boolean)
+class Tag(val name: String, var isFavorite: Boolean, val creation: Date)
+class Subscription(val name: String, val startID: Int, var currentID: Int)

76cec95a0f4d9696fcb160ea2d7a941e0b3eed4e
Yochyo <herodragon25@gmail.com>
2019-01-28 13:14:07 +0100

    you can now scroll between different pictures in picture_activity

--- app/src/main/java/de/yochyo/yBooru/layout/PictureActivity.kt
+++ app/src/main/java/de/yochyo/yBooru/layout/PictureActivity.kt
@@ -3,16 +3,17 @@ package de.yochyo.yBooru.layout
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.*
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
-import de.yochyo.yBooru.GestureListener
 import de.yochyo.yBooru.R
 import de.yochyo.yBooru.api.Api
-import de.yochyo.yBooru.api.Post
 import de.yochyo.yBooru.api.Tag
 import de.yochyo.yBooru.file.FileManager
 import de.yochyo.yBooru.manager.Manager
@@ -33,16 +34,31 @@ class PictureActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture)
         setSupportActionBar(toolbar_picture)
-        initGestureListener()
 
         m = Manager.get(intent.getStringExtra("tags"))
         nav_view_picture.bringToFront()
         recycleView = nav_view_picture.getHeaderView(0).findViewById(R.id.recycle_view_info)
-        recycleView.adapter = Adapter()
+        recycleView.adapter = InfoAdapter()
         recycleView.layoutManager = LinearLayoutManager(this)
-        val post = m.currentPost
-        if (post != null) {
-            loadImage(post)
+        with(view_pager) {
+            adapter = PageAdapter()
+            currentItem = m.position
+            val p = m.currentPost
+            if (p != null) {
+                currentTags.apply { clear();addAll(p.tagsCopyright);addAll(p.tagsArtist); addAll(p.tagsCharacter); addAll(p.tagsGeneral); addAll(p.tagsMeta) }
+                recycleView.adapter?.notifyDataSetChanged()
+            }
+            addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+                override fun onPageScrollStateChanged(p0: Int) {}
+                override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {}
+                override fun onPageSelected(p0: Int) {
+                    val post = m.dataSet[p0]
+                    if (post != null) {
+                        currentTags.apply { clear();addAll(post.tagsCopyright);addAll(post.tagsArtist); addAll(post.tagsCharacter); addAll(post.tagsGeneral); addAll(post.tagsMeta) }
+                        recycleView.adapter?.notifyDataSetChanged()
+                    }
+                }
+            })
         }
     }
 
@@ -62,55 +78,38 @@ class PictureActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadImage(post: Post) {
-        currentTags.apply { clear();addAll(post.tagsCopyright);addAll(post.tagsArtist); addAll(post.tagsCharacter); addAll(post.tagsGeneral); addAll(post.tagsMeta) }
-        recycleView.adapter?.notifyDataSetChanged()
-        GlobalScope.launch {
-            val preview = Api.downloadImage(this@PictureActivity, post.filePreviewURL, "${post.id}Preview")
-            launch(Dispatchers.Main) { image_view.setImageBitmap(preview) }
-            launch {
-                val bitmap = Api.downloadImage(this@PictureActivity, post.fileLargeURL, "${post.id}Large")
-                launch(Dispatchers.Main) { image_view.setImageBitmap(bitmap) }
-            }
-        }
-    }
-
-    private fun initGestureListener() {
-        val gestureDetector = GestureDetector(this, object : GestureListener() {
-            override fun onSwipe(direction: Direction): Boolean {
-                if (direction == Direction.right) {
-                    if (m.position >= 1) {
-                        m.position -= 1
-                        val post = m.currentPost
-                        if (post != null)
-                            loadImage(post)
-                    }
-                } else if (direction == Direction.left) {
-                    if (m.position < m.dataSet.lastIndex) {
-                        m.position += 1
-                        val post = m.currentPost
-                        if (post != null)
-                            loadImage(post)
-                    }
-                }
-                return false
-            }
-        })
-        image_view.setOnTouchListener(object : View.OnTouchListener {
-            override fun onTouch(v: View, event: MotionEvent?): Boolean {
-                v.onTouchEvent(event)
-                gestureDetector.onTouchEvent(event)
-                return true
-            }
-        })
-    }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.picture_menu, menu)
         return true
     }
 
-    private inner class Adapter : RecyclerView.Adapter<InfoButtonHolder>() {
+    private inner class PageAdapter : PagerAdapter() {
+        override fun isViewFromObject(p0: View, p1: Any): Boolean = p0 == p1
+        override fun getCount(): Int = m.dataSet.size
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val imageView = LayoutInflater.from(this@PictureActivity).inflate(R.layout.picture_item_view, container, false) as ImageView
+            val post = m.dataSet[position]
+            if (post != null) {
+                GlobalScope.launch {
+                    val preview = Api.downloadImage(this@PictureActivity, post.filePreviewURL, "${post.id}Preview")
+                    launch(Dispatchers.Main) { imageView.setImageBitmap(preview) }
+                    launch {
+                        val bitmap = Api.downloadImage(this@PictureActivity, post.fileLargeURL, "${post.id}Large")
+                        launch(Dispatchers.Main) { imageView.setImageBitmap(bitmap) }
+                    }
+                }
+                container.addView(imageView)
+            }
+            return imageView
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            //TODO was soll ich machen
+        }
+
+    }
+
+    private inner class InfoAdapter : RecyclerView.Adapter<InfoButtonHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InfoButtonHolder = InfoButtonHolder(LayoutInflater.from(parent.context).inflate(R.layout.info_item_button, parent, false) as Button)
 
         override fun getItemCount(): Int = currentTags.size

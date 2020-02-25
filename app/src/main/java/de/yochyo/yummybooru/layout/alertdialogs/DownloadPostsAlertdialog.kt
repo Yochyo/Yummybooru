10eea83045817038ba9c1bd6f0d81b63a18f308c
Yochyo <herodragon25@gmail.com>
2020-02-25 14:12:19 +0100

    implemented download all

--- app/src/main/java/de/yochyo/yummybooru/layout/alertdialogs/DownloadPostsAlertdialog.kt
+++ app/src/main/java/de/yochyo/yummybooru/layout/alertdialogs/DownloadPostsAlertdialog.kt
@@ -2,15 +2,25 @@ package de.yochyo.yummybooru.layout.alertdialogs
 
 import android.app.AlertDialog
 import android.content.Context
-import android.widget.Toast
+import de.yochyo.yummybooru.api.entities.Server
+import de.yochyo.yummybooru.downloadservice.DownloadService
 import de.yochyo.yummybooru.utils.Manager
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class DownloadPostsAlertdialog(context: Context, manager: Manager) {
     init {
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Download all (attention)")
-        builder.setMessage("All! pictures will be downloaded, make sure you have enough storage space")
-        Toast.makeText(context, "Not implemented yet because of security reasons", Toast.LENGTH_SHORT).show()
+        builder.setMessage("Make sure you have enough storage space")
+        builder.setPositiveButton("Download") { _, _ ->
+            GlobalScope.launch {
+                while (manager.loadNextPage(context)?.isNotEmpty() == true) {
+
+                }
+                DownloadService.startService(context, manager, Server.getCurrentServer(context))
+            }
+        }
         builder.show()
     }
 }

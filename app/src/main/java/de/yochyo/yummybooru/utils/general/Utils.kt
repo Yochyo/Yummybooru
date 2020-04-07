b8970e9542080ad5215e32bb98f3640af3d026c3
Yochyo <herodragon25@gmail.com>
2020-04-07 20:56:56 +0200

    remove unused function

--- app/src/main/java/de/yochyo/yummybooru/utils/general/Utils.kt
+++ app/src/main/java/de/yochyo/yummybooru/utils/general/Utils.kt
@@ -98,17 +98,6 @@ suspend fun createTagAndOrChangeSubState(context: Context, name: String): de.yoc
     return null
 }
 
-fun passwordToHash(password: String): String {
-    val byteArray = "choujin-steiner--$password--".toByteArray(charset = Charsets.UTF_8)
-    val digest = MessageDigest.getInstance("SHA-1")
-    digest.update(byteArray)
-    val digestBytes = digest.digest()
-    val digestStr = StringBuilder()
-    for (b in digestBytes)
-        digestStr.append(String.format("%02x", b))
-    return digestStr.toString()
-}
-
 fun parseURL(url: String): String {
     val b = StringBuffer()
     if (!url.startsWith("http"))

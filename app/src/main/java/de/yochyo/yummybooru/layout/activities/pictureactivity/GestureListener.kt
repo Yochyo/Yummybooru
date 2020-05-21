98b71cd9b18b5f8c843ca4bc8fe97b7e8c36cc50
Yochyo <herodragon25@gmail.com>
2020-05-21 17:41:46 +0200

    can now zoom in PictureActivity again

--- app/src/main/java/de/yochyo/yummybooru/layout/activities/pictureactivity/GestureListener.kt
+++ app/src/main/java/de/yochyo/yummybooru/layout/activities/pictureactivity/GestureListener.kt
@@ -7,36 +7,41 @@ abstract class GestureListener : GestureDetector.SimpleOnGestureListener() {
     abstract fun onSwipe(direction: Direction): Boolean
 
     override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-        return onSwipe(getDirection(e1.x, e1.y, e2.x, e2.y))
+        return onSwipe(getDirection(e1, e2))
     }
 
     override fun onDown(e: MotionEvent?): Boolean {
         return true
     }
 
-    private fun getDirection(x1: Float, y1: Float, x2: Float, y2: Float): Direction {
-        val angle = getAngle(x1, y1, x2, y2)
-        return Direction.fromAngle(angle)
-    }
+    companion object {
+        fun getDirection(e1: MotionEvent, e2: MotionEvent): Direction {
+            return getDirection(e1.x, e1.y, e2.x, e2.y)
+        }
 
-    private fun getAngle(x1: Float, y1: Float, x2: Float, y2: Float): Double {
-        val rad = Math.atan2((y1 - y2).toDouble(), (x2 - x1).toDouble()) + Math.PI
-        return (rad * 180 / Math.PI + 180) % 360
+        fun getDirection(x1: Float, y1: Float, x2: Float, y2: Float): Direction {
+            val angle = getAngle(x1, y1, x2, y2)
+            return fromAngle(angle)
+        }
+
+        fun fromAngle(angle: Double): Direction {
+            return if (inRange(angle, 45f, 135f)) Direction.UP
+            else if (inRange(angle, 0f, 45f) || inRange(angle, 315f, 360f)) Direction.RIGHT
+            else if (inRange(angle, 225f, 315f)) Direction.DOWN
+            else Direction.LEFT
+        }
+
+        private fun getAngle(x1: Float, y1: Float, x2: Float, y2: Float): Double {
+            val rad = Math.atan2((y1 - y2).toDouble(), (x2 - x1).toDouble()) + Math.PI
+            return (rad * 180 / Math.PI + 180) % 360
+        }
+
+
+        private fun inRange(angle: Double, init: Float, end: Float): Boolean = angle >= init && angle < end
     }
 
 
     enum class Direction {
-        Up, Down, Left, Right;
-
-        companion object {
-            fun fromAngle(angle: Double): Direction {
-                return if (inRange(angle, 45f, 135f)) Up
-                else if (inRange(angle, 0f, 45f) || inRange(angle, 315f, 360f)) Right
-                else if (inRange(angle, 225f, 315f)) Down
-                else Left
-            }
-
-            private fun inRange(angle: Double, init: Float, end: Float): Boolean = angle >= init && angle < end
-        }
+        UP, DOWN, LEFT, RIGHT;
     }
 }
--- app/src/main/java/de/yochyo/yummybooru/layout/activities/pictureactivity/PictureAdapter.kt
+++ app/src/main/java/de/yochyo/yummybooru/layout/activities/pictureactivity/PictureAdapter.kt
@@ -1,12 +1,10 @@
 package de.yochyo.yummybooru.layout.activities.pictureactivity
 
-import android.view.GestureDetector
-import android.view.Gravity
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.PagerAdapter
+import com.github.chrisbanes.photoview.OnSingleFlingListener
 import com.github.chrisbanes.photoview.PhotoView
 import com.google.android.material.snackbar.Snackbar
 import de.yochyo.booruapi.objects.Post
@@ -56,39 +54,16 @@ class PictureAdapter(val activity: AppCompatActivity, val m: ManagerWrapper) : P
         view.tag = position
         return view
     }
+
     private fun createView(post: Post, position: Int): View {
+        var lastSwipeUp = 0L
+
         fun forImage(): View {
             val view = PhotoView(activity)
             view.setAllowParentInterceptOnEdge(true)
-            GlobalScope.launch {
-                try {
-                    val preview = activity.cache.getCachedFile(activity.preview(post.id))
-                    if (preview != null) launch(Dispatchers.Main) { preview.loadIntoImageView(view) }
-                    download(activity, post.fileSampleURL, activity.sample(post.id), { GlobalScope.launch(Dispatchers.Main) { it.loadIntoImageView(view) } }, downloadFirst = true, cacheFile = true)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
-            return view
-        }
+            view.setOnSingleFlingListener(object : OnSingleFlingListener {
 
-        fun forVideo(): View {
-            val layout = LinearLayout(activity)
-            layout.gravity = Gravity.CENTER
-            layout.orientation = LinearLayout.HORIZONTAL
-            layout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT)
-            val view = MediaView(activity)
-            layout.addView(view)
-            view.setVideoPath(post.fileSampleURL)
-            if(m.position == position) view.resume() //if first page is video, it would not play
-            return layout
-        }
-
-        var lastSwipeUp = 0L
-
-        val detector = GestureDetector(activity, object : GestureListener() {
-            override fun onSwipe(direction: Direction): Boolean {
-                return if (direction == Direction.Up) {
+                fun onFlingUp(position: Int) {
                     val time = System.currentTimeMillis()
                     val p = m.posts.elementAt(position)
                     if (time - lastSwipeUp > 400L) { //download
@@ -106,19 +81,85 @@ class PictureAdapter(val activity: AppCompatActivity, val m: ManagerWrapper) : P
                         }
                     }
                     lastSwipeUp = time
-                    true
-                } else if (direction == Direction.Down) {
-                    activity.finish()
-                    true
-                } else false
+                }
+
+                override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+                    return when (GestureListener.getDirection(e1, e2)) {
+                        GestureListener.Direction.DOWN -> {
+                            activity.finish()
+                            true
+                        }
+                        GestureListener.Direction.UP -> {
+                            onFlingUp(position)
+                            true
+                        }
+                        else -> return false
+                    }
+                }
+            })
+            GlobalScope.launch {
+                try {
+                    val preview = activity.cache.getCachedFile(activity.preview(post.id))
+                    if (preview != null) launch(Dispatchers.Main) { preview.loadIntoImageView(view) }
+                    download(activity, post.fileSampleURL, activity.sample(post.id), { GlobalScope.launch(Dispatchers.Main) { it.loadIntoImageView(view) } }, downloadFirst = true, cacheFile = true)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
-        })
+            return view
+        }
+
+        fun forVideo(): View {
+            val layout = LinearLayout(activity)
+            layout.gravity = Gravity.CENTER
+            layout.orientation = LinearLayout.HORIZONTAL
+            layout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            val view = MediaView(activity)
+            layout.addView(view)
+            view.setVideoPath(post.fileSampleURL)
+            if (m.position == position) view.resume() //if first page is video, it would not play
+
+            val detector = GestureDetector(activity, object : GestureListener() {
+                override fun onSwipe(direction: Direction): Boolean {
+                    return when (direction) {
+                        Direction.UP -> {
+                            val time = System.currentTimeMillis()
+                            val p = m.posts.elementAt(position)
+                            if (time - lastSwipeUp > 400L) { //download
+                                downloadImage(activity, p)
+                                val snack = Snackbar.make(activity.view_pager, activity.getString(R.string.download), Snackbar.LENGTH_SHORT)
+                                snack.show()
+                                GlobalScope.launch(Dispatchers.Main) {
+                                    delay(150)
+                                    snack.dismiss()
+                                }
+                            } else { //double swipe
+                                GlobalScope.launch {
+                                    for (tag in p.tags.filter { it.type == Tag.ARTIST })
+                                        db.tags += tag.toBooruTag(activity)
+                                }
+                            }
+                            lastSwipeUp = time
+                            true
+                        }
+                        Direction.DOWN -> {
+                            activity.finish()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+            })
+            layout.setOnTouchListener { _, event -> detector.onTouchEvent(event) }
+
+            return layout
+        }
 
         val view = when (Resource.typeFromMimeType(post.fileSampleURL.mimeType ?: "")) {
             Resource.VIDEO -> forVideo()
             else -> forImage()
         }
-        view.setOnTouchListener { _, event -> detector.onTouchEvent(event) }
+
         return view
     }
 

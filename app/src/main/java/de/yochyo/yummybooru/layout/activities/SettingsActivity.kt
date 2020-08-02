88548d93804d03e1ba55640145620ddb8985382f
Yochyo <herodragon25@gmail.com>
2020-08-02 19:51:04 +0200

    can now use backpress menuitem

--- app/src/main/java/de/yochyo/yummybooru/layout/activities/SettingsActivity.kt
+++ app/src/main/java/de/yochyo/yummybooru/layout/activities/SettingsActivity.kt
@@ -2,13 +2,14 @@ package de.yochyo.yummybooru.layout.activities
 
 import android.app.Activity
 import android.content.Intent
-import de.yochyo.yummybooru.R
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.documentfile.provider.DocumentFile
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import de.yochyo.yummybooru.R
 import de.yochyo.yummybooru.backup.BackupUtils
 import de.yochyo.yummybooru.database.db
 import de.yochyo.yummybooru.updater.AutoUpdater
@@ -23,10 +24,21 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.layout, MySettingsFragment())
                 .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }

8479c87ac81ada3d770f32ad58c2305de00f5c2b
Yochyo <herodragon25@gmail.com>
2020-08-14 19:45:23 +0200

    fixed bug where app crashed when moving fast between zoomed in images

---
+++ app/src/main/java/de/yochyo/yummybooru/layout/views/custom/ViewPagerFixed.kt
@@ -0,0 +1,27 @@
+package de.yochyo.yummybooru.layout.views.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+
+class ViewPagerFixed(context: Context, attrs: AttributeSet? = null) : ViewPager(context, attrs) {
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        try {
+            return super.onTouchEvent(ev)
+        } catch (ex: IllegalArgumentException) {
+            ex.printStackTrace()
+        }
+        return false
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        try {
+            return super.onInterceptTouchEvent(ev)
+        } catch (ex: IllegalArgumentException) {
+            ex.printStackTrace()
+        }
+        return false
+    }
+}

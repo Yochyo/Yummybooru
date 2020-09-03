e7f1ec1cddb32afc715919f699fa830c68ce8aa2
Yochyo <herodragon25@gmail.com>
2020-09-03 15:21:58 +0200

    update BooruApi dependency -> add MyImouto api, added server templates in AddServerDialog.kt

--- app/src/main/java/de/yochyo/yummybooru/api/Apis.kt
+++ app/src/main/java/de/yochyo/yummybooru/api/Apis.kt
@@ -1,17 +1,19 @@
 package de.yochyo.yummybooru.api
 
-import de.yochyo.booruapi.api.DanbooruApi
-import de.yochyo.booruapi.api.GelbooruApi
-import de.yochyo.booruapi.api.IBooruApi
-import de.yochyo.booruapi.api.MoebooruApi
+import de.yochyo.booruapi.api.*
 import java.util.*
 
 object Apis {
-    val apis = arrayListOf("danbooru", "moebooru", "gelbooru")
+    val DANBOORU = "Danbooru"
+    val MOEBOORU = "Moebooru"
+    val GELBOORU = "Gelbooru"
+    val MY_IMOUTO = "MyImouto"
+    val apis = arrayListOf(DANBOORU, MOEBOORU, GELBOORU, MY_IMOUTO)
     fun getApi(name: String, url: String): IBooruApi {
         return when (name.toLowerCase(Locale.ENGLISH)) {
-            "moebooru" -> return MoebooruApi(url)
-            "gelbooru" -> return GelbooruApi(url)
+            MOEBOORU -> return MoebooruApi(url)
+            GELBOORU -> return GelbooruApi(url)
+            MY_IMOUTO -> MyImoutoApi(url)
             else -> DanbooruApi(url)
         }
     }
--- app/src/main/java/de/yochyo/yummybooru/layout/alertdialogs/AddServerDialog.kt
+++ app/src/main/java/de/yochyo/yummybooru/layout/alertdialogs/AddServerDialog.kt
@@ -15,56 +15,88 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class AddServerDialog(val runOnPositive: (s: Server) -> Unit) {
-    var server = Server("", "", "", "", "")
+    var server = Server("", "", "")
     var title: String? = null
 
     fun withServer(server: Server) = apply { this.server = server }
     fun withTitle(s: String) = apply { title = s }
 
-    fun build(context: Context) {
-        val builder = AlertDialog.Builder(context)
-        val layout = LayoutInflater.from(context).inflate(R.layout.add_server_dialog_view, null) as LinearLayout
-        builder.setView(layout)
-        val spinner = layout.findViewById<Spinner>(R.id.add_server_api)
-        val adapter = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1)
-        adapter.add("auto")
-        adapter.addAll(Apis.apis)
-        spinner.adapter = adapter
-        builder.setTitle(title ?: context.getString(R.string.add_server))
 
+    private var layout: LinearLayout? = null
+    val serverName: String get() = layout!!.findViewById<TextView>(R.id.add_server_name).text.toString()
+    val url: String get() = layout!!.findViewById<TextView>(R.id.add_server_url).text.toString()
+    val username: String get() = layout!!.findViewById<TextView>(R.id.add_server_username).text.toString()
+    val password: String get() = layout!!.findViewById<TextView>(R.id.add_server_password).text.toString()
+    val apiName: String get() = layout!!.findViewById<Spinner>(R.id.add_server_api).selectedItem.toString()
+    private fun fillLayoutAndCreate(context: Context, server: Server): LinearLayout {
+        val layout = if (layout == null) {
+            val layout = LayoutInflater.from(context).inflate(R.layout.add_server_dialog_view, null) as LinearLayout
+            this.layout = layout
+
+            val spinner = layout.findViewById<Spinner>(R.id.add_server_api)
+            val adapter = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1)
+            adapter.add("auto")
+            adapter.addAll(Apis.apis)
+            spinner.adapter = adapter
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val item = spinner.selectedItem.toString()
+                    layout.findViewById<TextView>(R.id.add_server_password).hint = when (item) {
+                        "auto" -> context.getString(R.string.hint_password_or_api_key)
+                        Apis.DANBOORU -> context.getString(R.string.hint_api_key)
+                        else -> context.getString(R.string.hint_password)
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+
+            val templateSpinner = layout.findViewById<Spinner>(R.id.server_template)
+            val templateSpinnerAdapter = ArrayAdapter<Server>(context, android.R.layout.simple_list_item_1)
+            templateSpinnerAdapter.add(Server("Template", "", ""))
+            templateSpinnerAdapter.addAll(ServerTemplates.templates)
+            templateSpinner.adapter = templateSpinnerAdapter
+            templateSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val selectedServer = templateSpinner.selectedItem as Server
+                    if (selectedServer.name != "Template")
+                        fillLayoutAndCreate(context, selectedServer)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+
+            layout
+        } else this.layout!!
+
+        val spinner = layout.findViewById<Spinner>(R.id.add_server_api)
         for (spin in 0 until spinner.adapter.count)
             if ((spinner.adapter.getItem(spin) as String).equals(server.apiName, true)) {
                 spinner.setSelection(spin)
                 break
             }
 
+        layout.findViewById<TextView>(R.id.add_server_name).text = server.name
+        layout.findViewById<TextView>(R.id.add_server_url).text = server.url
+        layout.findViewById<TextView>(R.id.add_server_username).text = server.username
+        layout.findViewById<TextView>(R.id.add_server_password).text = server.password
 
-        val url = layout.findViewById<TextView>(R.id.add_server_url).apply { text = server.url }
-        val username = layout.findViewById<TextView>(R.id.add_server_username).apply { text = server.username }
-        val password = layout.findViewById<TextView>(R.id.add_server_password).apply { text = server.password }
+        return layout
+    }
 
-        val name = layout.findViewById<TextView>(R.id.add_server_name).apply { text = server.name }
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val item = spinner.selectedItem.toString()
-                password.hint = when (item) {
-                    "auto" -> context.getString(R.string.hint_password_or_api_key)
-                    "danbooru" -> context.getString(R.string.hint_api_key)
-                    else -> context.getString(R.string.hint_password)
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        }
+    fun build(context: Context) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(title ?: context.getString(R.string.add_server))
+        builder.setView(fillLayoutAndCreate(context, server))
 
         builder.setPositiveButton(context.getString(R.string.positive_button_name)) { _, _ ->
             GlobalScope.launch(Dispatchers.IO) {
                 try {
-                    val s = Server(name.text.toString(), parseURL(url.text.toString()), spinner.selectedItem.toString(), username.text.toString(),
-                            password.text.toString(), id = server.id)
-                    if (s.apiName == "Auto") s.apiName = getCorrectApi(context, s)
+                    val s = Server(
+                        serverName, parseURL(url), apiName, username,
+                        password, id = server.id
+                    )
+                    if (s.apiName == "auto") s.apiName = getCorrectApi(s)
                     if (s.newestID() == null)
                         withContext(Dispatchers.Main) { Toast.makeText(context, context.getString(R.string.bad_login), Toast.LENGTH_LONG).show() }
                     withContext(Dispatchers.Main) { runOnPositive(s) }
@@ -78,10 +110,22 @@ class AddServerDialog(val runOnPositive: (s: Server) -> Unit) {
     }
 
 
-    private suspend fun getCorrectApi(context: Context, s: Server): String {
+    private suspend fun getCorrectApi(s: Server): String {
         for (api in Apis.apis) {
             if (s.newestID() != 0) return api
         }
         return Apis.apis.first()
+    }
+}
+
+private object ServerTemplates {
+    val templates = ArrayList<Server>()
+
+    init {
+        templates += Server("Danbooru", "https://danbooru.donmai.us/", Apis.DANBOORU)
+        templates += Server("Konachan", "https://konachan.com/", Apis.MOEBOORU)
+        templates += Server("Yande.re", "https://yande.re/", Apis.MOEBOORU)
+        templates += Server("Gelbooru", "https://gelbooru.com/", Apis.GELBOORU)
+        templates += Server("Lolibooru", "https://lolibooru.moe/", Apis.MY_IMOUTO)
     }
 }

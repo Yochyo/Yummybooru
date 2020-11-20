d83d2b22ca698b0af97ed9f5ca4553ece75776d5
Yochyo <herodragon25@gmail.com>
2020-11-20 16:49:17 +0100

    added MyImouto Support

--- app/src/main/java/de/yochyo/yummybooru/api/Apis.kt
+++ app/src/main/java/de/yochyo/yummybooru/api/Apis.kt
@@ -4,6 +4,7 @@ import de.yochyo.booruapi.api.IBooruApi
 import de.yochyo.booruapi.api.danbooru.DanbooruApi
 import de.yochyo.booruapi.api.gelbooru.GelbooruApi
 import de.yochyo.booruapi.api.moebooru.MoebooruApi
+import de.yochyo.booruapi.api.my_imouto.MyImoutoApi
 import java.util.*
 
 object Apis {
@@ -14,9 +15,9 @@ object Apis {
     val apis = arrayListOf(DANBOORU, MOEBOORU, GELBOORU, MY_IMOUTO)
     fun getApi(name: String, url: String): IBooruApi {
         return when (name.toLowerCase(Locale.ENGLISH)) {
-            MOEBOORU.toLowerCase(Locale.ENGLISH) -> return MoebooruApi(url)
-            GELBOORU.toLowerCase(Locale.ENGLISH) -> return GelbooruApi(url)
-            //TODO  MY_IMOUTO.toLowerCase(Locale.ENGLISH) -> MyImoutoApi(url)
+            MOEBOORU.toLowerCase(Locale.ENGLISH) -> MoebooruApi(url)
+            GELBOORU.toLowerCase(Locale.ENGLISH) -> GelbooruApi(url)
+            MY_IMOUTO.toLowerCase(Locale.ENGLISH) -> MyImoutoApi(url)
             else -> DanbooruApi(url)
         }
     }

b09da8321a4a7ba5cd1919dc456d4c579bb18e6b
Yochyo <herodragon25@gmail.com>
2020-12-03 11:10:34 +0100

    app will now longer crash if jsoup (BooruApi) crashes

--- app/src/main/java/de/yochyo/yummybooru/updater/Changelog.kt
+++ app/src/main/java/de/yochyo/yummybooru/updater/Changelog.kt
@@ -12,21 +12,27 @@ class Changelog(val versionName: String, val version: Int, val description: Stri
         init {
             logs += Changelog("1.0", 0, "- First Version")
             logs += Changelog("1.1", 1, "- Bug fixes")
-            logs += Changelog("1.3", 2, "- You can now download all pictures of a type automatically\n" +
-                    "- application got faster\n" +
-                    "- some bug fixes\n" +
-                    "- preparation for future updates")
+            logs += Changelog(
+                "1.3", 2, "- You can now download all pictures of a type automatically\n" +
+                        "- application got faster\n" +
+                        "- some bug fixes\n" +
+                        "- preparation for future updates"
+            )
             logs += Changelog("1.3.1", 3, "- Some small bugfixes")
-            logs += Changelog("1.4", 4, "- App can now be used starting Android 5.0 Lollipop\n" +
-                    "- Fixed a lot of small bugs (most of them did not impact the user experience)\n" +
-                    "- Editing/Favoriting/... tags or subs is now more convenient than ever\n" +
-                    "- You can now multiselect pictures and download them all.\n" +
-                    "- Some visual changes\n" +
-                    "- Optimized scrolling experience\n" +
-                    "- Double swipe up a picture to add the artists to the tag history")
-            logs += Changelog("1.5", 5, "- You can now filter tags in the startscreen (Thanks to Water_LP for suggesting it in Discord)\n" +
-                    "- You can now update several Subscriptions directly and more comfortable\n" +
-                    "- App will now update automatically when an new version is out (Yay)")
+            logs += Changelog(
+                "1.4", 4, "- App can now be used starting Android 5.0 Lollipop\n" +
+                        "- Fixed a lot of small bugs (most of them did not impact the user experience)\n" +
+                        "- Editing/Favoriting/... tags or subs is now more convenient than ever\n" +
+                        "- You can now multiselect pictures and download them all.\n" +
+                        "- Some visual changes\n" +
+                        "- Optimized scrolling experience\n" +
+                        "- Double swipe up a picture to add the artists to the tag history"
+            )
+            logs += Changelog(
+                "1.5", 5, "- You can now filter tags in the startscreen (Thanks to Water_LP for suggesting it in Discord)\n" +
+                        "- You can now update several Subscriptions directly and more comfortable\n" +
+                        "- App will now update automatically when an new version is out (Yay)"
+            )
             logs += Changelog(
                 "2.0", 6, "- You can now see the changelog in the settings\n" +
                         "- Gifs can now be displayed and downloaded\n" +
@@ -61,6 +67,15 @@ class Changelog(val versionName: String, val version: Int, val description: Stri
                         "Added a lot of new settings\n" +
                         "Faster cache"
             )
+            logs += Changelog(
+                "2.4", 11, "Added support for gelbooru beta\n" +
+                        "Changed color of unknown tags to cyan\n" +
+                        "App will now display an error if download fails\n" +
+                        "Changed how left/right swipes are registered by the app\n" +
+                        "Can now hide download toasts\n" +
+                        "Bug fixes\n"
+            )
+            logs += Changelog("2.4.1", 12, "Bug fixes")
         }
 
         fun showChangelogs(context: Context) {

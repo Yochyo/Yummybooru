10afc89d38bcd8a48aee034fb4ae44015240d17e
Yochyo <herodragon25@gmail.com>
2021-01-26 17:31:20 +0100

    updated dependencies

--- app/src/main/java/de/yochyo/yummybooru/api/manager/ManagerWrapper.kt
+++ app/src/main/java/de/yochyo/yummybooru/api/manager/ManagerWrapper.kt
@@ -7,7 +7,7 @@ import de.yochyo.booruapi.manager.ManagerBuilder
 import de.yochyo.eventcollection.events.OnAddElementsEvent
 import de.yochyo.yummybooru.database.db
 
-class ManagerWrapper(private val manager: IManager) : IManager by manager {
+open class ManagerWrapper(private val manager: IManager) : IManager by manager {
     val currentPost: Post?
         get() {
             return if (position in 0 until posts.size) posts[position]
@@ -17,19 +17,14 @@ class ManagerWrapper(private val manager: IManager) : IManager by manager {
 
     var reachedLastPage = false
 
-    override fun toString() = manager.toString()
-
-    override suspend fun downloadNextPage(): List<Post>? {
-        return downloadNextPages(1)
-    }
-
     override suspend fun downloadNextPages(amount: Int): List<Post>? {
         val result = manager.downloadNextPages(amount)
         if (result != null && result.isEmpty()) posts.triggerOnAddElementsEvent(OnAddElementsEvent(posts, emptyList()))
-
         reachedLastPage = result?.isEmpty() == true
         return result
     }
+
+    override fun toString() = manager.toString()
 
     companion object {
         fun build(context: Context, s: String) = ManagerWrapper(ManagerBuilder.createManager(context.db.currentServer.api, s, context.db.limit))

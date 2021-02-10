9e8ed181fc1acc39bae121d0fff907a1f28ec3e3
Philipp <herodragon25@gmail.com>
2021-02-10 20:58:20 +0100

    changed snack duration to 2000 -> 2500ms

--- app/src/main/java/de/yochyo/yummybooru/utils/commands/Command.kt
+++ app/src/main/java/de/yochyo/yummybooru/utils/commands/Command.kt
@@ -13,7 +13,7 @@ interface Command {
         suspend fun execute(view: View, command: Command, showSnackbar: Boolean = command.showSnackbarDefault): Boolean {
             val res = command.run(view.context)
             if (res) {
-                val snack = Snackbar.make(view, command.getUndoMessage(view.context), 2000)
+                val snack = Snackbar.make(view, command.getUndoMessage(view.context), 2500)
                 if (showSnackbar) {
                     snack.setAction("Undo") { GlobalScope.launch { command.undo(view.context) } }
                     withContext(Dispatchers.Main) { snack.show() }
